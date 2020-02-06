239cb44b61dcfc10aa71f10b15c6c79ff4f25a29
tejing <tejing163@msn.com>
2020-02-06 18:48:47 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,3 +7,126 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+
+if [ ! -d "./package/myapp" ];then
+mkdir ./package/myapp
+fi
+cd ./package/myapp
+
+echo "koolproxyR"
+if [ ! -d "./koolproxyR" ];then
+git clone https://github.com/tzxiaozhen88/koolproxyR.git
+else
+cd koolproxyR
+git pull
+cd ..
+fi
+
+echo "luci-app-adguardhome"
+if [ ! -d "./luci-app-adguardhome" ];then
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
+else
+cd luci-app-adguardhome
+git pull
+cd ..
+fi
+
+echo "luci-app-mentohust"
+if [ ! -d "./luci-app-mentohust" ];then
+git clone https://github.com/BoringCat/luci-app-mentohust.git
+else
+cd luci-app-mentohust
+git pull
+cd ..
+fi
+
+echo "MentoHUST-OpenWrt-ipk"
+if [ ! -d "./MentoHUST-OpenWrt-ipk" ];then
+git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git
+else
+cd MentoHUST-OpenWrt-ipk
+git pull
+cd ..
+fi
+
+echo "luci-theme-argon"
+if [ ! -d "./luci-theme-argon" ];then
+git clone https://github.com/jerrykuku/luci-theme-argon.git
+else
+cd luci-theme-argon
+git pull
+cd ..
+fi
+
+echo "luci-theme-atmaterial"
+if [ ! -d "./luci-theme-atmaterial" ];then
+git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git
+else
+cd luci-theme-atmaterial
+git pull
+cd ..
+fi
+
+echo "luci-theme-argon-mc"
+if [ ! -d "./luci-theme-argon-mc" ];then
+svn checkout https://github.com/project-openwrt/openwrt/trunk/package/lienol/luci-theme-argon-mc
+else
+cd luci-theme-argon-mc
+svn update
+cd ..
+fi
+
+
+echo "luci-theme-rosy"
+if [ ! -d "./luci-theme-rosy" ];then
+git clone https://github.com/rosywrt/luci-theme-rosy.git
+else
+cd luci-theme-rosy
+git pull
+cd ..
+fi
+
+echo "luci-theme-darkmatter"
+if [ ! -d "./luci-theme-darkmatter" ];then
+git clone https://github.com/apollo-ng/luci-theme-darkmatter.git
+else
+cd luci-theme-darkmatter
+git pull
+cd ..
+fi
+
+echo "luci-app-serverchan"
+if [ ! -d "./luci-app-serverchan" ];then
+svn checkout https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/luci-app-serverchan
+else
+cd luci-app-serverchan
+svn update
+cd ..
+fi
+
+echo "openwrt-mwol"
+if [ ! -d "./openwrt-mwol" ];then
+git clone https://github.com/Mleaf/openwrt-mwol.git
+else
+cd openwrt-mwol
+git pull
+cd ..
+fi
+
+echo "luci-app-openclash"
+if [ ! -d "./OpenClash" ];then
+mkdir ./OpenClash
+cd ./OpenClash
+git init
+git remote add -f origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull origin master
+git branch --set-upstream-to=origin/master master
+cd ..
+else
+cd ./OpenClash/luci-app-openclash
+git pull origin master
+cd ..
+cd ..
+fi
